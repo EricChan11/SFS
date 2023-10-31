1fc31b7e4d28f03e643ccc78a720248ec11505c6
CHEN KAIYANG <115172673+EricChan11@users.noreply.github.com>
2023-10-31 09:23:19 +0800

    new 2 files

---
+++ SFC.c
@@ -0,0 +1,43 @@
+struct sb {
+    long fs_size;  //文件系统的大小，以块为单位
+    long first_blk;  //数据区的第一块块号，根目录也放在此
+    long datasize;  //数据区大小，以块为单位 
+    long first_inode;    //inode区起始块号
+    long inode_area_size;   //inode区大小，以块为单位
+    long fisrt_blk_of_inodebitmap;   //inode位图区起始块号
+    long inodebitmap_size;  // inode位图区大小，以块为单位
+    long first_blk_of_databitmap;   //数据块位图起始块号
+    long databitmap_size;      //数据块位图大小，以块为单位
+};
+
+struct inode {//共64字节 
+    short int st_mode; /* 权限，2字节 */ 
+    short int st_ino; /* i-node号，2字节 */ 
+    char st_nlink; /* 连接数，1字节 */ 
+    uid_t st_uid; /* 拥有者的用户 ID ，4字节 */ 
+    gid_t st_gid; /* 拥有者的组 ID，4字节  */ 
+    off_t st_size; /*文件大小，4字节 */ 
+    struct timespec st_atim; /* 16个字节time of last access */ 
+    short int addr [7];    /*磁盘地址，14字节*/
+};
+
+struct directory{//共16字节
+    char name [8];
+    char expand [3];
+    short int st_ino; /* i-node号，2字节 */ 
+    //3字节备用
+
+};
+
+stastic struct fuse_operations SFS_opener{
+    .init = SFS_init,
+    .getattr = SFS_getattr,
+    .readdir = SFS_readdir,
+    .mkdir = SFS_mkdir,
+    .rmdir = SFS_rmdir,
+    .mknod = SFS_mkknod,
+    .write = SFS_write,
+    .read = SFS_read,
+    .unlink = SFS_unlink,
+
+};
